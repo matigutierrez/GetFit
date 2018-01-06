d3d4a4467570e2b88b8d8e16c12f3103e4e8c656
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2018-01-06 17:50:30 -0300

    Migration Plan

---
+++ Backend/database/migrations/2018_01_06_204439_create_plan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgf_plan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tgf_sede_id')->unsigned();
+            $table->foreign('tgf_sede_id')->references('id')->on('tgf_sede')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('pla_nombre');
+            $table->string('pla_descripcion');
+            $table->integer('pla_costo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgf_plan');
+    }
+}
