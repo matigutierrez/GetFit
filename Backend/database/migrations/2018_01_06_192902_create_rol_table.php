38ab12cfefc247d04d45d1802a89d8af119098ef
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-06 16:37:45 -0300

    Migracion Roles

---
+++ Backend/database/migrations/2018_01_06_192902_create_rol_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgf_rol', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('rol_nombre');
+            $table->text('rol_descripcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgf_rol');
+    }
+}
