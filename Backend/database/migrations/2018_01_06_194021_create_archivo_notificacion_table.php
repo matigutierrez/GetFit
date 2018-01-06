d5ab49759d8213c8bf88cc6f8c2fb35a4404db92
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2018-01-06 17:02:14 -0300

    Migration ArchivoNotificacion

---
+++ Backend/database/migrations/2018_01_06_194021_create_archivo_notificacion_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArchivoNotificacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgf_archivo_notificacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tgf_notificion_id')->unsigned();
+            $table->foreign('tgf_notificion_id')->references('id')->on('tgf_notificacion')->onDelete('cascade')->onUpdate('cascade');
+            $table->binary('arn_archivo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgf_archivo_notificacion');
+    }
+}
