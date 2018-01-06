1d25997319f0d87b59d4c44f051d992054a3dd2f
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-06 17:42:33 -0300

    Migración EvaluacionArchivo

---
+++ Backend/database/migrations/2018_01_06_203945_create_evaluacion_archivo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluacionArchivoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgf_evaluacion_archivo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tgf_evaluacion_id')->unsigned();
+            $table->foreign('tgf_evaluacion_id')->references('id')->on('tgf_evaluacion')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('ear_nombre');
+            $table->binary('ear_archivo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgf_evaluacion_archivo');
+    }
+}
