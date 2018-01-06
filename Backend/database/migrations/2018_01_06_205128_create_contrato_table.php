c3677cff50ebb17d790335d013624ec0cae2f43b
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-06 17:56:23 -0300

    Migración Contrato

---
+++ Backend/database/migrations/2018_01_06_205128_create_contrato_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContratoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgf_contrato', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tgf_cliente_id')->unsigned();
+            $table->foreign('tgf_cliente_id')->references('id')->on('tgf_cliente')->onDelete('cascade')->onUpdate('delete');
+            $table->timestamp('con_fecha_inicio');
+            $table->binary('con_acta');                 // Esta columna debe ser de tipo mediumblob
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgf_contrato');
+    }
+}
