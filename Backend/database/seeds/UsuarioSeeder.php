16a18d4a5b1c733bb4ca15b15263712849e3dc6a
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2018-01-07 16:35:24 -0300

    Correction

--- Backend/database/seeds/UsuarioSeeder.php
+++ Backend/database/seeds/UsuarioSeeder.php
@@ -12,8 +12,8 @@ class UsuarioSeeder extends Seeder
     public function run()
     {
         DB::table('tgf_usuario')->insert([
-          'usu_rol' => '1',
-          'usu_cliente' => '1',
+          'tgf_rol_id' => '1',
+          'tgf_cliente_id' => '1',
           'usu_correo' => 'm.gutierrez06@ufromail.cl',
           'usu_pass' => bcrypt('1234'),
         ]);
