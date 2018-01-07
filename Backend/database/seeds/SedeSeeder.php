0fc7abd2d4b6377cb5c3a3fff3a23ab790aa61ba
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-07 16:31:05 -0300

    Sede Seeder

--- Backend/database/seeds/SedeSeeder.php
+++ Backend/database/seeds/SedeSeeder.php
@@ -11,6 +11,9 @@ class SedeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tgf_sede')->insert([
+        	['sed_nombre'=>'Gimnasio Trinidad Zúñiga', 'sed_direccion'=>'Al lado del Unimarc'],
+        	['sed_nombre'=>'Gimnasio para Hombres', 'sed_direccion'=>'Todavia no existe'],
+        ]);
     }
 }
