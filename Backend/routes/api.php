aee6d4eedcff7e09d1d8d40e0d6cadf5e68595e6
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-23 16:57:52 -0300

    Agregando rutas

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -14,10 +14,24 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['jwt.auth']], function () {
-
-	Route::apiResource('usuario', 'UsuarioController');
+	Route::apiResource('archivoEvaluacion', 'ArchivoEvaluacionController');
+	Route::apiResource('archivoNotificacion', 'ArchivoNotificacionController');
+	Route::apiResource('asistencia', 'AsistenciaController');
+	Route::apiResource('cliente', 'ClienteController');
+	Route::apiResource('cobranza', 'CobranzaController');
+	Route::apiResource('contrato', 'ContratoController');
+	Route::apiResource('descuentoCobranza', 'DescuentoCobranzaController');
+	Route::apiResource('descuentoPlan', 'DescuentoPlanController');
+	Route::apiResource('evaluacion', 'EvaluacionController');
+	Route::apiResource('foto', 'FotoController');
+	Route::apiResource('horario', 'HorarioController');
+	Route::apiResource('metodopago', 'MetodoPagoController');
+	Route::apiResource('notificacion', 'NotificacionController');
+	Route::apiResource('pago', 'PagoController');
+	Route::apiResource('plan', 'PlanController');
+	Route::apiResource('rol', 'RolController');
 	Route::apiResource('sede', 'SedeController');
-
+	Route::apiResource('usuario', 'UsuarioController');
 });
 
-Route::post('login', 'AuthController@authenticate');
+Route::post('login', 'AuthenticateController@authenticate');
