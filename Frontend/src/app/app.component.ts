0246c0b4c2163fe078906ea794ad2ed98e458025
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2018-03-25 16:36:04 -0300

    Modificacion

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,57 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './services/user.service';
-import { ClienteService } from './services/cliente.service';
-import { RolService } from './services/rol.service';
-declare var $:any;
-declare var jQuery:any;
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService, ClienteService, RolService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public identity;
-  public token;
-  public user;
-  public admin: boolean;
-  public cliente: boolean;
 
-  constructor(
-  	private _userService: UserService,
-    private _clienteService: ClienteService,
-    private _rolService: RolService
-
-  ){
-  	this.identity = this._userService.getIdentity();
-    this.token = this._userService.getToken();
-    if(this.token != undefined){
-      this._rolService.getRolSesion().subscribe(
-        Response => {
-          this.user = Response;
-          if(this.user.rol_nombre == "Administrador"){
-            this.admin = true;
-          }else{
-            this.cliente = true;
-          }
-        },
-        Error => {
-          console.log(<any>Error)
-        }
-      );
-    }
-  }
+  constructor(){}
 
   ngOnInit(){
-    this.ajusteLogin();
+
   }
 
-  ajusteLogin(){
-    if (!this.identity && !this.token) {
-      $("main").css("padding-left", "0px");
-      $("header").css("padding-left", "0px");
-    }
-  }
 }
