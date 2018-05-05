80ae2bf295aa1147e875a0883a68a86d6b95ae72
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2018-05-05 19:28:08 -0300

    Plan

--- Frontend/src/app/components/modadmin/planes/plan/plan.component.ts
+++ Frontend/src/app/components/modadmin/planes/plan/plan.component.ts
@@ -1,6 +1,8 @@
 import { PlanService } from "../../../../services/plan.service";
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Plan } from "../../../../models/Plan";
+import { HorarioComponent } from "../../../extra/horario/horario.component";
 
 @Component({
     selector: 'plan',
@@ -9,19 +11,34 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class PlanComponent implements OnInit {
 
+    @ViewChild(HorarioComponent)
+    public horario: HorarioComponent;
+
     public id: number;
+    public plan: Plan = new Plan();
 
     public constructor(
         private _planService: PlanService,
         private _route: ActivatedRoute
     ) {
-
-    }
-
-    public ngOnInit() {
         this._route.params.subscribe(params => {
             this.id = params["id"];
         });
     }
+
+    public ngOnInit() {
+
+        this._planService.get(this.id).subscribe(
+            Response => {
+                this.plan = Response;
+            }
+        )
+    }
+
+    public abrirHorarios() {
+    
+        this.horario.abrir([this.plan]);
+    
+      }
 
 }
