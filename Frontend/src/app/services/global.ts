8766c05450ab84a7573c9f46a08070448e2e3392
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-05-20 17:09:22 -0400

    Cambiando IP host

--- Frontend/src/app/services/global.ts
+++ Frontend/src/app/services/global.ts
@@ -1,5 +1,5 @@
 // 198.58.101.50
-export var GLOBAL = {
-	host: 'localhost:8000',
-	url: 'http://localhost:8000/api/'
-};
+export var GLOBAL: any = {};
+
+GLOBAL.host = '198.58.101.50:8000';
+GLOBAL.url + 'http://' + GLOBAL.host + '/api/';
